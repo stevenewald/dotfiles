54809b42fd551a47e748eb79b88cc8c8ce36d21d
stevenewald <stevenewald6@gmail.com>
2024-12-17 02:59:47 -0500

    Refactored nvim filestructure

--- nvim/.config/nvim/lua/lazy_nvim.lua
+++ nvim/.config/nvim/lua/lazy_nvim.lua
@@ -1,27 +1,34 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
-require("lazy").setup("plugins", {
-  defaults = { lazy = true, version = false },
-  ui = { wrap = "true" },
-  change_detection = { enabled = true },
-  checker = {enabled = false},
-  performance = {
-    rtp = {
-      disabled_plugins = {
-      },
-    },
-  },
+require("lazy").setup({
+	spec = {
+		{ import = "plugins.lsp" },
+		{ import = "plugins.util" },
+		{ import = "plugins.visual" },
+		{ import = "plugins.navigation" },
+		{ import = "plugins.external" },
+	},
+	defaults = { lazy = true, version = false },
+	ui = { wrap = "true" },
+	change_detection = { enabled = true },
+	checker = { enabled = false },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+			},
+		},
+	},
 })
